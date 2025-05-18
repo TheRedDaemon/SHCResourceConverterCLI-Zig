be9e1e3610417e2071a98795cc426fccab9491bc
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2025-05-18 15:47:28 +0200

    [GM1] fix blt and add more options, remove contains checks

--- src/coder/blt.zig
+++ src/coder/blt.zig
@@ -2,20 +2,15 @@ const std = @import("std");
 
 const BltError = error{
     InvalidDataSize,
-    DoesNotFitTarget,
-    InvalidBitMaskSize,
-};
-
-const CopyError = error{
-    InvalidDataSize,
-    TargetOutsideSource,
     InvalidBitMaskSize,
 };
 
 const CopyInstructionFieldName = enum {
-    require_contained,
-    ignore_value,
-    bit_mask,
+    position_mode,
+    source_ignore_value,
+    source_bit_mask,
+    target_ignore_value,
+    target_bit_mask,
 };
 
 const DefaultValues = struct {
@@ -23,31 +18,59 @@ const DefaultValues = struct {
     pub const @"false" = false;
 };
 
+pub const PositionMode = enum {
+    source,
+    target,
+};
+
 pub fn CopyInstruction(
-    require_contained: bool,
-    ignore_value: ?type,
-    bit_mask: bool,
+    position_mode: PositionMode,
+    source_ignore_value: ?type,
+    source_bit_mask: bool,
+    target_ignore_value: ?type,
+    target_bit_mask: bool,
 ) type {
     var fields: []const std.builtin.Type.StructField = &.{};
     fields = fields ++ &[_]std.builtin.Type.StructField{.{
-        .name = @tagName(CopyInstructionFieldName.require_contained),
-        .type = bool,
-        .default_value_ptr = if (require_contained) &DefaultValues.true else &DefaultValues.false,
+        .name = @tagName(CopyInstructionFieldName.position_mode),
+        .type = PositionMode,
+        .default_value_ptr = switch (position_mode) {
+            .source => &PositionMode.source,
+            .target => &PositionMode.target,
+        },
         .is_comptime = true,
         .alignment = 0,
     }};
-    if (ignore_value) |value_type| {
+    if (source_ignore_value) |value_type| {
         fields = fields ++ &[_]std.builtin.Type.StructField{.{
-            .name = @tagName(CopyInstructionFieldName.ignore_value),
+            .name = @tagName(CopyInstructionFieldName.source_ignore_value),
             .type = value_type,
             .default_value_ptr = null,
             .is_comptime = false,
             .alignment = 0,
         }};
     }
-    if (bit_mask) {
+    if (target_ignore_value) |value_type| {
         fields = fields ++ &[_]std.builtin.Type.StructField{.{
-            .name = @tagName(CopyInstructionFieldName.bit_mask),
+            .name = @tagName(CopyInstructionFieldName.target_ignore_value),
+            .type = value_type,
+            .default_value_ptr = null,
+            .is_comptime = false,
+            .alignment = 0,
+        }};
+    }
+    if (source_bit_mask) {
+        fields = fields ++ &[_]std.builtin.Type.StructField{.{
+            .name = @tagName(CopyInstructionFieldName.source_bit_mask),
+            .type = []const u1,
+            .default_value_ptr = null,
+            .is_comptime = false,
+            .alignment = 0,
+        }};
+    }
+    if (target_bit_mask) {
+        fields = fields ++ &[_]std.builtin.Type.StructField{.{
+            .name = @tagName(CopyInstructionFieldName.target_bit_mask),
             .type = []const u1,
             .default_value_ptr = null,
             .is_comptime = false,
@@ -68,255 +91,151 @@ pub fn blt(
     comptime T: type,
     instruction: anytype,
     source: []const T,
-    source_height: usize,
     source_width: usize,
-    destination: []T,
-    destination_height: usize,
-    destination_width: usize,
+    source_height: usize,
+    target: []T,
+    target_width: usize,
+    target_height: usize,
     position_x: isize,
     position_y: isize,
 ) BltError!void {
-    if (source.len != source_width * source_height or destination.len != destination_width * destination_height) {
+    if (source.len != source_width * source_height or target.len != target_width * target_height) {
         return BltError.InvalidDataSize;
     }
 
     const InstructionType = if (@typeInfo(@TypeOf(instruction)) == .pointer) std.meta.Child(@TypeOf(instruction)) else @TypeOf(instruction);
 
-    const x_start, const x_end, const y_start, const y_end, const x_start_source, const y_start_source = if (instruction.require_contained) blk: {
-        const intern_x_end = position_x + @as(isize, @intCast(source_width));
-        const intern_y_end = position_y + @as(isize, @intCast(source_height));
+    const source_position_x, const source_position_y = switch (instruction.position_mode) {
+        .source => .{ -position_x, -position_y },
+        .target => .{ position_x, position_y },
+    };
 
-        if (position_x < 0 or
-            position_y < 0 or
-            intern_x_end > destination_width or
-            intern_y_end > destination_height)
-        {
-            return BltError.DoesNotFitTarget;
-        }
-        break :blk .{
-            @as(usize, @intCast(position_x)),
-            @as(usize, @intCast(intern_x_end)),
-            @as(usize, @intCast(position_y)),
-            @as(usize, @intCast(intern_y_end)),
-            0,
-            0,
-        };
-    } else blk: {
-        const intern_x_start_source = if (position_x < 0) @abs(position_x) else 0;
-        const intern_y_start_source = if (position_y < 0) @abs(position_y) else 0;
+    const x_start_target, const x_end_target, const y_start_target, const y_end_target, const x_start_source, const y_start_source = blk: {
+        const intern_x_start_source = if (source_position_x < 0) @abs(source_position_x) else 0;
+        const intern_y_start_source = if (source_position_y < 0) @abs(source_position_y) else 0;
 
         break :blk .{
-            if (intern_x_start_source > 0) 0 else @as(usize, @intCast(position_x)),
+            if (intern_x_start_source > 0) 0 else @as(usize, @intCast(source_position_x)),
             value: {
-                const intern_end: isize = position_x + @as(isize, @intCast(source_width));
-                break :value if (intern_end > destination_width) destination_width else @as(usize, @intCast(intern_end));
+                const intern_end: isize = source_position_x + @as(isize, @intCast(source_width));
+                break :value if (intern_end > target_width) target_width else @as(usize, @intCast(intern_end));
             },
-            if (intern_y_start_source > 0) 0 else @as(usize, @intCast(position_y)),
+            if (intern_y_start_source > 0) 0 else @as(usize, @intCast(source_position_y)),
             value: {
-                const intern_end: isize = position_x + @as(isize, @intCast(source_height));
-                break :value if (intern_end > destination_height) destination_height else @as(usize, @intCast(intern_end));
+                const intern_end: isize = source_position_y + @as(isize, @intCast(source_height));
+                break :value if (intern_end > target_height) target_height else @as(usize, @intCast(intern_end));
             },
             intern_x_start_source,
             intern_y_start_source,
         };
     };
 
-    const ignore_value = if (@hasField(InstructionType, @tagName(CopyInstructionFieldName.ignore_value))) instruction.ignore_value;
-    const bit_mask = blk: {
-        if (!@hasField(InstructionType, @tagName(CopyInstructionFieldName.bit_mask))) {
+    const source_ignore_value = if (@hasField(InstructionType, @tagName(CopyInstructionFieldName.source_ignore_value))) instruction.source_ignore_value;
+    const source_bit_mask = blk: {
+        if (!@hasField(InstructionType, @tagName(CopyInstructionFieldName.source_bit_mask))) {
             break :blk;
         }
-        if (instruction.bit_mask.len != source.len) {
+        if (instruction.source_bit_mask.len != source.len) {
             return BltError.InvalidBitMaskSize;
         }
-        break :blk instruction.bit_mask;
+        break :blk instruction.source_bit_mask;
+    };
+    const target_ignore_value = if (@hasField(InstructionType, @tagName(CopyInstructionFieldName.target_ignore_value))) instruction.target_ignore_value;
+    const target_bit_mask = blk: {
+        if (!@hasField(InstructionType, @tagName(CopyInstructionFieldName.target_bit_mask))) {
+            break :blk;
+        }
+        if (instruction.target_bit_mask.len != target.len) {
+            return BltError.InvalidBitMaskSize;
+        }
+        break :blk instruction.target_bit_mask;
     };
 
-    if (@typeInfo(@TypeOf(ignore_value)) == .void and @typeInfo(@TypeOf(bit_mask)) == .void) {
-        for (y_start..y_end, y_start_source..) |y_target, y_source| {
-            const lines_target = y_target * destination_width;
+    if (@typeInfo(@TypeOf(source_ignore_value)) == .void and
+        @typeInfo(@TypeOf(target_ignore_value)) == .void and
+        @typeInfo(@TypeOf(source_bit_mask)) == .void and
+        @typeInfo(@TypeOf(target_bit_mask)) == .void)
+    {
+        for (y_start_target..y_end_target, y_start_source..) |y_target, y_source| {
+            const lines_target = y_target * target_width;
             const lines_source = y_source * source_width;
             @memcpy(
-                destination[lines_target + x_start .. lines_target + x_end],
-                source[lines_source + x_start_source .. lines_source + x_start_source + x_end - x_start],
+                target[lines_target + x_start_target .. lines_target + x_end_target],
+                source[lines_source + x_start_source .. lines_source + x_start_source + x_end_target - x_start_target],
             );
         }
         return;
     }
 
-    for (y_start..y_end, y_start_source..) |y_target, y_source| {
-        const lines_target = y_target * destination_width;
+    for (y_start_target..y_end_target, y_start_source..) |y_target, y_source| {
+        const lines_target = y_target * target_width;
         const lines_source = y_source * source_width;
-        for (x_start..x_end, x_start_source..) |x_target, x_source| {
+        for (x_start_target..x_end_target, x_start_source..) |x_target, x_source| {
             const index_target = lines_target + x_target;
             const index_source = lines_source + x_source;
 
-            if (@typeInfo(@TypeOf(ignore_value)) != .void) {
-                if (source[index_source] == ignore_value) {
+            if (@typeInfo(@TypeOf(source_ignore_value)) != .void) {
+                if (source[index_source] == source_ignore_value) {
                     continue;
                 }
             }
 
-            if (@typeInfo(@TypeOf(bit_mask)) != .void) {
-                if (bit_mask[index_source] == 0) {
+            if (@typeInfo(@TypeOf(source_bit_mask)) != .void) {
+                if (source_bit_mask[index_source] == 0) {
                     continue;
                 }
             }
 
-            destination[index_target] = source[index_source];
+            if (@typeInfo(@TypeOf(target_ignore_value)) != .void) {
+                if (target[index_target] == target_ignore_value) {
+                    continue;
+                }
+            }
+
+            if (@typeInfo(@TypeOf(target_bit_mask)) != .void) {
+                if (target_bit_mask[index_target] == 0) {
+                    continue;
+                }
+            }
+
+            target[index_target] = source[index_source];
         }
     }
 }
 
-// pub fn copy(
-//     comptime T: type,
-//     comptime instruction: anytype,
-//     allocator: std.mem.Allocator,
-//     source: []const T,
-//     source_height: usize,
-//     source_width: usize,
-//     destination: []T,
-//     destination_height: usize,
-//     destination_width: usize,
-//     position_x: isize,
-//     position_y: isize,
-// ) BltError!void {
-//     const InstructionType = if (@typeInfo(@TypeOf(instruction)) == .pointer) std.meta.Child(@TypeOf(instruction)) else @TypeOf(instruction);
-
-//     const x_start, const x_end, const y_start, const y_end, const x_start_source, const y_start_source = if (instruction.require_contained) blk: {
-//         const intern_x_end = position_x + @as(isize, @intCast(source_width));
-//         const intern_y_end = position_y + @as(isize, @intCast(source_height));
-
-//         if (position_x < 0 or
-//             position_y < 0 or
-//             intern_x_end > destination_width or
-//             intern_y_end > destination_height)
-//         {
-//             return BltError.DoesNotFitTarget;
-//         }
-//         break :blk .{
-//             @as(usize, @intCast(position_x)),
-//             @as(usize, @intCast(intern_x_end)),
-//             @as(usize, @intCast(position_y)),
-//             @as(usize, @intCast(intern_y_end)),
-//             0,
-//             0,
-//         };
-//     } else blk: {
-//         const intern_x_start_source = if (position_x < 0) @abs(position_x) else 0;
-//         const intern_y_start_source = if (position_y < 0) @abs(position_y) else 0;
-
-//         break :blk .{
-//             if (intern_x_start_source > 0) 0 else @as(usize, @intCast(position_x)),
-//             value: {
-//                 const intern_end: isize = position_x + @as(isize, @intCast(source_width));
-//                 break :value if (intern_end > destination_width) destination_width else @as(usize, @intCast(intern_end));
-//             },
-//             if (intern_y_start_source > 0) 0 else @as(usize, @intCast(position_y)),
-//             value: {
-//                 const intern_end: isize = position_x + @as(isize, @intCast(source_height));
-//                 break :value if (intern_end > destination_height) destination_height else @as(usize, @intCast(intern_end));
-//             },
-//             intern_x_start_source,
-//             intern_y_start_source,
-//         };
-//     };
-
-//     const ignore_value = if (@hasField(InstructionType, @tagName(CopyInstructionFieldName.ignore_value))) instruction.ignore_value;
-//     const bit_mask = blk: {
-//         if (!@hasField(InstructionType, @tagName(CopyInstructionFieldName.bit_mask))) {
-//             break :blk;
-//         }
-//         if (instruction.bit_mask.len != source.len) {
-//             return BltError.InvalidBitMaskSize;
-//         }
-//         break :blk instruction.bit_mask;
-//     };
-
-//     if (@typeInfo(@TypeOf(ignore_value)) == .void and @typeInfo(@TypeOf(bit_mask)) == .void) {
-//         for (y_start..y_end, y_start_source..) |y_target, y_source| {
-//             const lines_target = y_target * destination_width;
-//             const lines_source = y_source * source_width;
-//             @memcpy(
-//                 destination[lines_target + x_start .. lines_target + x_end],
-//                 source[lines_source + x_start_source .. lines_source + x_start_source + x_end - x_start],
-//             );
-//         }
-//         return;
-//     }
-
-//     for (y_start..y_end, y_start_source..) |y_target, y_source| {
-//         const lines_target = y_target * destination_width;
-//         const lines_source = y_source * source_width;
-//         for (x_start..x_end, x_start_source..) |x_target, x_source| {
-//             const index_target = lines_target + x_target;
-//             const index_source = lines_source + x_source;
-
-//             if (@typeInfo(@TypeOf(ignore_value)) != .void) {
-//                 if (source[index_source] == ignore_value) {
-//                     continue;
-//                 }
-//             }
-
-//             if (@typeInfo(@TypeOf(bit_mask)) != .void) {
-//                 if (bit_mask[index_source] == 0) {
-//                     continue;
-//                 }
-//             }
-
-//             destination[index_target] = source[index_source];
-//         }
-//     }
-// }
-
 test "blt" {
     const source_width: usize = 2;
     const source_height: usize = 2;
-    const destination_width: usize = 3;
-    const destination_height: usize = 3;
+    const target_width: usize = 3;
+    const target_height: usize = 3;
 
     const source: []u8 = try std.testing.allocator.alloc(u8, source_width * source_height);
     defer std.testing.allocator.free(source);
     @memset(source, 1);
-    const target: []u8 = try std.testing.allocator.alloc(u8, destination_width * destination_height);
+    const target: []u8 = try std.testing.allocator.alloc(u8, target_width * target_height);
     defer std.testing.allocator.free(target);
     @memset(target, 0);
 
-    const Contained = CopyInstruction(true, null, false);
-    const contained_instruction: Contained = .{};
-
-    const contained_error = blt(
-        u8,
-        &contained_instruction,
-        source,
-        source_height,
-        source_width,
-        target,
-        destination_height,
-        destination_width,
-        -1,
-        -1,
-    );
-    try std.testing.expectError(BltError.DoesNotFitTarget, contained_error);
+    const Simple = CopyInstruction(PositionMode.target, null, false, null, false);
 
     @memset(target, 0);
     try blt(
         u8,
-        &contained_instruction,
+        Simple{},
         source,
         source_height,
         source_width,
         target,
-        destination_height,
-        destination_width,
+        target_height,
+        target_width,
         0,
         0,
     );
     try std.testing.expectEqualSlices(u8, &.{ 1, 1, 0, 1, 1, 0, 0, 0, 0 }, target);
 
-    const Ignored = CopyInstruction(false, u8, false);
+    const Ignored = CopyInstruction(PositionMode.target, u8, false, null, false);
     const ignored_instruction: Ignored = .{
-        .ignore_value = 1,
+        .source_ignore_value = 1,
     };
 
     @memset(target, 0);
@@ -327,14 +246,14 @@ test "blt" {
         source_height,
         source_width,
         target,
-        destination_height,
-        destination_width,
+        target_height,
+        target_width,
         0,
         0,
     );
     try std.testing.expectEqualSlices(u8, &.{ 0, 0, 0, 0, 0, 0, 0, 0, 0 }, target);
 
-    const Masked = CopyInstruction(false, null, true);
+    const Masked = CopyInstruction(PositionMode.target, null, true, null, false);
 
     var bit_mask: []u1 = try std.testing.allocator.alloc(u1, source_width * source_height);
     defer std.testing.allocator.free(bit_mask);
@@ -344,7 +263,7 @@ test "blt" {
     bit_mask[3] = 1;
 
     const masked_instruction: Masked = .{
-        .bit_mask = bit_mask,
+        .source_bit_mask = bit_mask,
     };
 
     @memset(target, 0);
@@ -355,10 +274,54 @@ test "blt" {
         source_height,
         source_width,
         target,
-        destination_height,
-        destination_width,
+        target_height,
+        target_width,
         2,
         1,
     );
     try std.testing.expectEqualSlices(u8, &.{ 0, 0, 0, 0, 0, 1, 0, 0, 0 }, target);
+
+    const copy_source_width: usize = target_width;
+    const copy_source_height: usize = target_height;
+    const copy_target_width: usize = source_width;
+    const copy_target_height: usize = source_height;
+
+    const copy_source = target;
+    const copy_target = source;
+
+    const SimpleCopy = CopyInstruction(PositionMode.source, null, false, null, false);
+
+    @memset(copy_source, 1);
+    @memset(copy_target, 0);
+    try blt(
+        u8,
+        SimpleCopy{},
+        copy_source,
+        copy_source_width,
+        copy_source_height,
+        copy_target,
+        copy_target_width,
+        copy_target_height,
+        2,
+        -1,
+    );
+    try std.testing.expectEqualSlices(u8, &.{ 0, 0, 1, 0 }, copy_target);
+
+    const IgnoreTarget = CopyInstruction(PositionMode.source, null, false, u8, false);
+
+    @memset(copy_source, 1);
+    @memset(copy_target, 0);
+    try blt(
+        u8,
+        IgnoreTarget{ .target_ignore_value = 0 },
+        copy_source,
+        copy_source_width,
+        copy_source_height,
+        copy_target,
+        copy_target_width,
+        copy_target_height,
+        0,
+        0,
+    );
+    try std.testing.expectEqualSlices(u8, &.{ 0, 0, 0, 0 }, copy_target);
 }
